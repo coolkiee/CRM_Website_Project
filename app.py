ef2f4afc9e074b48a8d6c3b55cac99dfb809ee7e
Coolkie <75506758+coolkiee@users.noreply.github.com>
2025-08-05 06:09:34 +0200

    04.08.2025

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
 
 # Flask uygulamasını oluşturuyoruz. __name__ özel bir Python değişkenidir
 # ve Flask'a uygulamanın nerede olduğunu söyler.
@@ -6,6 +8,21 @@ app = Flask(__name__)
 
 # Bu, bir URL'nin ("/") nasıl bir fonksiyonla eşleştiğini tanımlar.
 # Kullanıcı web sitemizin ana sayfasını ziyaret ettiğinde bu fonksiyon çalışacak.
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///crm.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db= SQLAlchemy(app)
+
+class Musteri(db.Model):
+    id=db.Column(db.Integer, primary_key=True)
+    sirket_adi = db.Column(db.String(100), nullable=False)
+    yetkili_kisi = db.Column(db.String(100))
+    email = db.Column(db.String(100), unique=True ,nullable=False)
+    telefon= db.Column(db.String(20))
+
+    def __repr__(self):
+        return f'<Manager {self.sirket_adi}>'
 @app.route('/')
 def anasayfa():
     return render_template('main_page.html')
